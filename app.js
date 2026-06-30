e2bc11dcb2bd34231cefabe04a3b8c6ed5a23810
govind-sudo <govindbhadoriya668@gmail.com>
2026-06-30 12:39:05 +0530

    qq
    ee

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 const express = require("express");
 const app = express();
 
+const path = require("path");
+const ejsmate = require("ejs-mate")
+
+// 1. MIDDLEWARE SETTINGS
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+app.engine('ejs', ejsmate);
+
+
+
+app.get("/students/dashboard",(req,res)=>{
+    res.render("student/dashboard");
+})
+
+
+app.get("/students/profile",(req,res)=>{
+    res.render("student/profile");
+})
+
+
+
+
 app.listen(3000,()=>{
     console.log("server is listing to 3000");
 })
