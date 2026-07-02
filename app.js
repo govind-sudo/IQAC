d919435126edbb1a75dd8b6b72c6037e8e73c567
Raghav Soni <181562819+RaghavXE@users.noreply.github.com>
2026-07-02 18:34:11 +0530

    .

--- app.js
+++ app.js
@@ -11,8 +11,10 @@ const mongoose = require("mongoose");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsmate = require("ejs-mate")
-
+// const mongoose = require("mongoose");
+const Student = require("./models/student");
 // 1. MIDDLEWARE SETTINGS
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 app.engine('ejs', ejsmate);
@@ -20,11 +22,35 @@ app.engine('ejs', ejsmate);
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.urlencoded({ extended: true }));
+// Mongo Connection
+const MONGO_URL = process.env.MONGO_URL;
 
+main()
+  .then(() => console.log("connected to db"))
+  .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
 
 app.get("/students/:id/dashboard", (req, res) => {
   res.render("students/dashboard", { currentPage: "dashboard" });
 });
+// app.get("/students/:id/dashboard", async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     const student = await Student.findById(id);
+
+//     if (!student) {
+//       return res.status(404).send("Student not found");
+//     }
+
+//     res.render("students/dashboard", { currentPage: "dashboard", student });
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).send("Something went wrong");
+//   }
+// });
 
 app.get("/students/:id/profile", (req, res) => {
   res.render("students/profile", { currentPage: "profile" });
