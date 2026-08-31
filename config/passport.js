a9cf7c82e43dac0c5015f521354dbfba18607368
govind-sudo <govindbhadoriya668@gmail.com>
2026-08-31 23:58:53 +0530

    add enrollment login debugging

--- config/passport.js
+++ config/passport.js
@@ -1,3 +1,24 @@
+// const passport = require('passport');
+// const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+// passport.use(
+//   new GoogleStrategy(
+//     {
+//       clientID: process.env.GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//       callbackURL: '/auth/google/callback',
+//     },
+//     (accessToken, refreshToken, profile, done) => {
+//       return done(null, profile);
+//     }
+//   )
+// );
+
+// passport.serializeUser((user, done) => done(null, user));
+// passport.deserializeUser((obj, done) => done(null, obj));
+
+// module.exports = passport;
+
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
@@ -6,8 +27,10 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: '/auth/google/callback',
+
+      callbackURL: 'https://iqac-student-portal.onrender.com/auth/google/callback',
     },
+
     (accessToken, refreshToken, profile, done) => {
       return done(null, profile);
     }
